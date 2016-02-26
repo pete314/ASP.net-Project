7b2756471eceb825159c59648ffd572af71c468f
pete314 <webpmaster@gmail.com>
2016-02-26 08:22:00 +0000

    Structural changes
    Assets - styling, javascript added along with structure
    Masterpage created

---
+++ ASP.net Project/assets/js/core.js
@@ -0,0 +1,14 @@
+﻿/*
+The core javascript for asp.net - Team Sergio
+Handles a default javascript events
+@author: Peter Nagy
+*/
+
+//The menu hover function
+$(document).ready(function () {
+    $("ul.top li").hover(function () {
+        $(this).children("ul.subsub").slideDown('fast');
+    }, function () {
+        $(this).children("ul.subsub").slideUp('slow');
+    });
+});
