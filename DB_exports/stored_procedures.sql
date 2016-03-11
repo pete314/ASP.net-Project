ea3f124e9223822f4d8379e48ce772e471d57848
pete314 <webpmaster@gmail.com>
2016-03-11 11:51:37 +0000

    Stored procedures updated

--- DB_exports/stored_procedures.sql
+++ DB_exports/stored_procedures.sql
@@ -28,3 +28,46 @@ Begin
   SELECT 0 as ReturnCode, NULL as Token, NULL as Email, NULL as Name
  End
 End
+
+-- Check if password token is valid
+-- Return user id if is valid
+Create Proc spTokenLookup 
+@TOKEN uniqueidentifier
+as
+Begin
+ Declare @UserId int
+ 
+ If(Exists(Select @UserId = user_id from password_reset_token where id = @TOKEN))
+ Begin
+  Select 1 as IsValidToken, @UserId as USER
+ End
+ Else
+ Begin
+  Select 0 as IsValidToken, @UserId as USER
+ End
+End
+
+-- Change user password
+Create Proc spChangeUserPassword
+@Password nvarchar(100),
+@UserId int,
+@GUID uniqueidentifier
+as
+Begin
+ if(@UserId is null)
+ Begin
+  Select 0 as IsPasswordChanged
+ End
+ Else
+ Begin
+  Update tblUsers set
+  [password] = @Password
+  where id = @UserId
+  
+  -- delete token
+  Delete from password_reset_token
+  where Id = @GUID
+  
+  Select 1 as IsPasswordChanged
+ End
+End
